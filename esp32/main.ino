14253439ae793ecfa0650ca2fdbdd455ce5e6540
nelsonfrz <nelson.frz@proton.me>
2023-11-10 21:03:36 +0100

    initial commit

---
+++ esp32/main.ino
@@ -0,0 +1,83 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <string>
+
+const char *ssid = "Wokwi-GUEST";
+const char *password = "";
+
+#define BTN_PIN 5
+#define TFT_DC 2
+#define TFT_CS 15
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+const String url = "https://194b-2a02-8071-4583-2a0-8d0d-b5f2-31e6-2222.ngrok-free.app/";
+
+int day = 6;
+int month = 11;
+int year = 2023;
+
+String getTimetable()
+{
+  HTTPClient http;
+  http.useHTTP10(true);
+  String fullUrl = url + "?day=" + String(day) + "&month=" + String(month) + "&year=" + String(year);
+  http.begin(fullUrl);
+  http.GET();
+  String result = http.getString();
+  result =
+      "Stundenplan " +
+      String(day) + "." + String(month) + "." + String(year) + ":\n" + result;
+
+  return result;
+}
+
+void nextTimetable()
+{
+  if (day < 31)
+  {
+    day++;
+  }
+  else
+  {
+    day = 1;
+    month++;
+  }
+
+  String timetable = getTimetable();
+  tft.println(timetable);
+}
+
+void setup()
+{
+  pinMode(BTN_PIN, INPUT_PULLUP);
+
+  WiFi.begin(ssid, password, 6);
+
+  tft.begin();
+  tft.setRotation(1);
+
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+  }
+
+  nextTimetable();
+}
+
+void loop()
+{
+  if (digitalRead(BTN_PIN) == LOW)
+  {
+    tft.fillScreen(ILI9341_BLACK);
+    tft.setCursor(0, 0);
+    nextTimetable();
+  }
+
+  delay(100);
+}
---
+++ index.ts
@@ -0,0 +1,64 @@
+import { WebUntis } from "webuntis";
+
+const untis = new WebUntis(
+  Bun.env.SCHOOL_NAME!,
+  Bun.env.USER_NAME!,
+  Bun.env.USER_PASSWORD!,
+  Bun.env.UNTIS_SERVER!
+);
+await untis.login();
+
+async function getTimetableString(day: number, month: number, year: number) {
+  let lessons = await untis.getOwnTimetableFor(new Date(year, month - 1, day));
+
+  if (!lessons) {
+    return "Sorry, no data found.";
+  }
+
+  lessons = lessons.sort((a, b) => a.startTime - b.startTime);
+
+  let timetable = lessons
+    .filter((lesson) => lesson.code !== "cancelled")
+    .map((lesson) => ({
+      ...lesson,
+      su: lesson.su.map((subject) => ({
+        ...subject,
+        longname: subject.longname.replace(", zweiter Parallelkurs", ""),
+      })),
+    }))
+    .map((lesson, index) =>
+      lesson.su.map((subject) => `${index + 1}. ${subject.longname}`).join(", ")
+    )
+    .join("\n");
+
+  return timetable;
+}
+
+Bun.serve({
+  port: 3000,
+  async fetch(req) {
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.search);
+    const day = searchParams.get("day");
+    const month = searchParams.get("month");
+    const year = searchParams.get("year");
+
+    if (!day || !month || !year) {
+      return new Response("day, month and year query params are obligatory.");
+    }
+
+    const parsedDay = parseInt(day);
+    const parsedMonth = parseInt(month);
+    const parsedYear = parseInt(year);
+
+    const timetable = await getTimetableString(
+      parsedDay,
+      parsedMonth,
+      parsedYear
+    );
+
+    console.log({ parsedDay, parsedMonth, parsedYear });
+
+    return new Response(timetable);
+  },
+});
